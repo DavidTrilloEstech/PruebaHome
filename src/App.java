4141c33449b7c5a29b4ffaebd6b5c966b4ab7fa3
David.Trillo.Gomez <davidtrillogomez@gmail.com>
2023-11-14 00:41:18 +0100

    prueba 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ha colao maquina");
+        System.out.println("ABCDEFGHIJKLMNOPQRSTUVWYZ");
     }
 }
